721c38e149d920a050e530917a51155899ce9669
LassNajmuldeen <lass.najm@gmail.com>
2024-12-21 03:34:01 +0100

    First TODO, called my image generation API and added a pathway for given prompts

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -8,6 +8,23 @@ export async function POST(request: Request) {
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
 
+    console.log(text);
+    const url = new URL("https://lassnajmuldeen--sd-turbo-model-generate.modal.run/");
+
+    // Adds given prompt to link
+    url.searchParams.set("prompt",text)
+
+    console.log("Requesting URL", url.toString());
+
+    // Create fetch request to the url
+    const response = await fetch(url.toString(),{
+      method: "GET",
+      headers: {
+        "X_API_Key": process.env.API_KEY || "",
+        Accept: "image/jpeg"
+      },
+    });
+
     return NextResponse.json({
       success: true,
       message: `Received: ${text}`,
